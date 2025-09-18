99628a33d9f82b6f715dc387396dccd6da14aa6d
Matt Kantor <the.matt.kantor@gmail.com>
2025-09-18 10:16:32 -0400

    Add `createElement` and accoutrements

---
+++ src/createElement.test.ts
@@ -0,0 +1,196 @@
+import assert from 'node:assert'
+import test, { suite } from 'node:test'
+import { ReadableStream } from 'web-streams-polyfill'
+import {
+  createElement,
+  trusted,
+  type PossiblyDeferredHTML,
+  type ReadableHTMLStream,
+} from './createElement.js'
+
+// TODO: Switch to `Array.fromAsync`.
+const arrayFromPossiblyDeferredHTML = async (source: PossiblyDeferredHTML) => {
+  const array = []
+  for await (const element of await source) {
+    array.push(element)
+  }
+  return array
+}
+
+suite('createElement', _ => {
+  test('empty element', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(createElement('a', {})),
+      ['<a>', '</a>'],
+    ))
+
+  test('element with text content', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(createElement('a', {}, 'a')),
+      ['<a>', 'a', '</a>'],
+    ))
+
+  test('element with string attribute', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('a', { href: 'https://example.com' }, 'a'),
+      ),
+      ['<a href="https://example.com">', 'a', '</a>'],
+    ))
+
+  test('element with element and non-element children', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('div', {}, 'a', createElement('div', {}, 'b'), 'c'),
+      ),
+      ['<div>', 'a', '<div>', 'b', '</div>', 'c', '</div>'],
+    ))
+
+  test('element with multiple separate text children', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('div', {}, 'a', 'b', 'c'),
+      ),
+      ['<div>', 'a', 'b', 'c', '</div>'],
+    ))
+
+  test('void elements', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(createElement('br', {})),
+      ['<br/>'],
+    ))
+
+  test('void element with attribute', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('img', {
+          src: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==',
+        }),
+      ),
+      [
+        '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="/>',
+      ],
+    ))
+
+  test('void element with escaped attribute', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('img', {
+          title: 'hello"world',
+        }),
+      ),
+      ['<img title="hello&quot;world"/>'],
+    ))
+
+  test('element with escaped text content', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(createElement('div', {}, '<&>')),
+      ['<div>', '&lt;&amp;&gt;', '</div>'],
+    ))
+
+  test('false boolean attribute', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('video', { autoplay: false }),
+      ),
+      ['<video>', '</video>'],
+    ))
+
+  test('true boolean attribute', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('video', { autoplay: true }),
+      ),
+      ['<video autoplay>', '</video>'],
+    ))
+
+  test('promise content', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('div', {}, Promise.resolve('<&>')),
+      ),
+      ['<div>', '&lt;&amp;&gt;', '</div>'],
+    ))
+
+  test('stream content', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('div', {}, ReadableStream.from(['<&>'])),
+      ),
+      ['<div>', '&lt;&amp;&gt;', '</div>'],
+    ))
+
+  test('trusted promise content', async _ => {
+    const trustedPromise: Promise<string> & { [trusted]?: true } =
+      Promise.resolve('<marquee>🕴</marquee>')
+    trustedPromise[trusted] = true
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('div', {}, trustedPromise),
+      ),
+      [
+        '<div>',
+        '<marquee>🕴</marquee>', // No escaping.
+        '</div>',
+      ],
+    )
+  })
+
+  test('trusted stream content', async _ => {
+    const trustedStream: ReadableHTMLStream = ReadableStream.from([
+      '<marquee>🕴</marquee>',
+    ])
+    trustedStream[trusted] = true
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('div', {}, trustedStream),
+      ),
+      [
+        '<div>',
+        '<marquee>🕴</marquee>', // No escaping.
+        '</div>',
+      ],
+    )
+  })
+
+  test('createElement', async _ =>
+    assert.deepEqual(
+      await arrayFromPossiblyDeferredHTML(
+        createElement('div', { class: 'a' }, 'b'),
+      ),
+      ['<div class="a">', 'b', '</div>'],
+    ))
+})
+
+// Type-level tests:
+try {
+  function FunctionWhichShouldNotBeUsableAsAComponent() {}
+  class ClassWhichShouldNotBeUsableAsAComponent {}
+
+  // @ts-expect-error
+  createElement(FunctionWhichShouldNotBeUsableAsAComponent, {})
+
+  // @ts-expect-error
+  createElement(ClassWhichShouldNotBeUsableAsAComponent, {})
+
+  // @ts-expect-error
+  createElement('br', {}, 'illegal')
+
+  // @ts-expect-error
+  createElement('non-existent-element', {})
+
+  // @ts-expect-error
+  createElement('button', { onclick: () => {} }) // Attribute values must be strings or booleans.
+
+  // @ts-expect-error
+  createElement('a', { nonexistentattribute: true })
+
+  // @ts-expect-error
+  createElement('a', { nonexistentattribute: 'value' })
+
+  // @ts-expect-error
+  createElement('a', { href: new URL() })
+
+  // @ts-expect-error
+  createElement('a', { 'non-existent-attribute': 'value' })
+} catch (_) {}
---
+++ src/createElement.ts
@@ -0,0 +1,119 @@
+import { ReadableStream } from 'web-streams-polyfill'
+import { stringifyAttributes, type AttributesByTagName } from './attributes.js'
+import { makeHTMLEscapingTransformStream } from './escaping.js'
+import { concatReadableStreams } from './readableStream.js'
+import {
+  isVoidElementTagName,
+  type VoidElementTagName,
+} from './voidElements.js'
+
+export type PossiblyDeferredHTML =
+  | string
+  | (Promise<string> & PossiblyTrusted)
+  | (AsyncIterable<string> & PossiblyTrusted)
+
+export type ReadableHTMLStream = ReadableStream<string> & PossiblyTrusted
+
+/**
+ * Children where `child[trusted] === true` are considered safe to emit without
+ * escaping.
+ */
+export const trusted = Symbol('trusted')
+
+/**
+ * Creates an HTML element from the given tag name, attributes, and children,
+ * returning a `ReadableStream`. Children may be supplied asynchronously as
+ * `Promise`s and/or async iterables.
+ *
+ * Attribute values and content are escaped via HTML entity encoding, except for
+ * children with a `trusted` symbol property set to `true`.
+ */
+export const createElement: (
+  ...[tagName, attributes, ...children]: CreateElementParameters
+) => ReadableHTMLStream = (
+  // This function gets called for fragments too. Direct callers of
+  // `createElement` shouldn't have to see the function parameter, so the
+  // externally-visible type above excludes `CreateFragmentParameters`.
+  ...[tagNameOrFragmentFunction, attributes, ...children]:
+    | CreateElementParameters
+    | CreateFragmentParameters
+) => {
+  let stream: ReadableHTMLStream
+
+  if (typeof tagNameOrFragmentFunction === 'function') {
+    stream = concatReadableStreams(children.map(escapeAsNeeded))
+  } else {
+    const stringifiedAttributes =
+      attributes === null ? '' : stringifyAttributes(attributes)
+
+    stream = isVoidElementTagName(tagNameOrFragmentFunction)
+      ? ReadableStream.from([
+          '<'.concat(tagNameOrFragmentFunction, stringifiedAttributes, '/>'),
+        ])
+      : concatReadableStreams([
+          ReadableStream.from([
+            '<'.concat(tagNameOrFragmentFunction, stringifiedAttributes, '>'),
+          ]),
+          ...children.map(escapeAsNeeded),
+          ReadableStream.from(['</'.concat(tagNameOrFragmentFunction, '>')]),
+        ])
+  }
+
+  stream[trusted] = true // Escaping has occurred.
+
+  return stream
+}
+
+type CreateElementParameters =
+  | {
+      [TagName in keyof AttributesByTagName]: readonly [
+        tagName: TagName,
+        attributes: AttributesByTagName[TagName] | null,
+        ...children: TagName extends VoidElementTagName
+          ? readonly []
+          : readonly PossiblyDeferredHTML[],
+      ]
+    }[keyof AttributesByTagName]
+
+type CreateFragmentParameters = readonly [
+  // With standard configuration this will be `createElement` itself.
+  component: (...parameters: never) => unknown,
+  attributes: null,
+  ...children: readonly PossiblyDeferredHTML[],
+]
+
+type PossiblyTrusted = { [trusted]?: true }
+
+const escapeAsNeeded = (element: PossiblyDeferredHTML): ReadableHTMLStream => {
+  const stream = elementAsReadableStream(element)
+
+  const elementIsTrusted =
+    typeof element === 'object' &&
+    trusted in element &&
+    element[trusted] === true
+
+  return elementIsTrusted
+    ? stream
+    : stream.pipeThrough(makeHTMLEscapingTransformStream())
+}
+
+const elementAsReadableStream = (
+  element: PossiblyDeferredHTML,
+): ReadableHTMLStream =>
+  new ReadableStream({
+    start: async controller => {
+      try {
+        const awaitedElement = await element
+        if (typeof awaitedElement === 'string') {
+          controller.enqueue(awaitedElement)
+        } else {
+          for await (const value of awaitedElement) {
+            controller.enqueue(value)
+          }
+        }
+        controller.close()
+      } catch (error) {
+        controller.error(error)
+      }
+    },
+  })
