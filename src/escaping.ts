8284780ec8ddbf9b4b5bf43f029e7ce07154a01b
Matt Kantor <the.matt.kantor@gmail.com>
2025-09-18 08:13:51 -0400

    Add `makeHTMLEscapingTransformStream`

--- src/escaping.ts
+++ src/escaping.ts
@@ -1,8 +1,18 @@
 import * as htmlEntities from 'html-entities'
+import { TransformStream } from 'web-streams-polyfill'
 
 export const escapeHTMLContent = (content: string): string =>
   htmlEntities.encode(content, {
     mode: 'specialChars',
     level: 'html5',
     numeric: 'hexadecimal',
+  })
+
+export const makeHTMLEscapingTransformStream = (): TransformStream<
+  string,
+  string
+> =>
+  new TransformStream({
+    transform: (chunk, controller) =>
+      controller.enqueue(escapeHTMLContent(chunk)),
   })
